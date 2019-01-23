8930bb4dd4968a20f761b594c3aa8e97950217fc
mtarlungeanu <mtarlungeanu@lenovo.com>
2019-01-23 12:53:33 +0200

    Now is finished :)

--- code/src/utils/itemGen.js
+++ code/src/utils/itemGen.js
@@ -5,34 +5,34 @@ const _items = [{
 }, {
 	name: 'Biscuits',
 	credit: 2.5,
-	url: 'https://png.pngtree.com/element_pic/17/03/22/2de2d82b4a67578ba9e51a91ed394cb2.jpg'
+	url: 'http://icons.iconarchive.com/icons/oxygen-icons.org/oxygen/128/Apps-preferences-web-browser-cookies-icon.png'
 }, {
 	name: 'Soda',
 	credit: 3,
-	url: 'https://c7.uihere.com/files/292/667/684/soft-drink-coca-cola-pepsi-sprite-coca-cola-drink-png-image-thumb.jpg'
+	url: 'http://icons.iconarchive.com/icons/musett/coca-cola/128/Coca-Cola-icon.png'
 }, {
 	name: 'Sandwich',
 	credit: 5,
-	url: 'https://png.pngtree.com/element_pic/00/16/07/2357933d72dbe8a.jpg'
+	url: 'http://icons.iconarchive.com/icons/google/noto-emoji-food-drink/128/32386-sandwich-icon.png'
 }, {
 	name: 'Croissant',
 	credit: 2,
-	url: 'https://www.clipartmax.com/png/middle/107-1077287_dd-croissant-by-amis0129-kawaii-croissant.png'
+	url: 'http://icons.iconarchive.com/icons/musett/coffee-shop/128/Croissant-icon.png'
 }, {
 	name: 'Candy',
 	credit: 1,
-	url: 'https://img.pngio.com/red-cartoon-candy-red-candy-cartoon-candy-candy-png-image-and-clipart-candy-png-260_261.png'
+	url: 'http://icons.iconarchive.com/icons/gianni-polito/colobrush/128/candy-stick-icon.png'
 }, {
 	name: 'Peanuts',
 	credit: 0.5,
-	url: 'https://png.pngtree.com/element_pic/16/12/31/6f6b92e43654c58f161ebd96969a1c01.jpg'
+	url: 'http://icons.iconarchive.com/icons/google/noto-emoji-food-drink/128/32369-peanuts-icon.png'
 }]
 
 export default function generateItems({
 	width: w = 4, // columns
 	height: h = 8, // rows
 	depth: d = 4,
-	variableDepth = true
+	variableDepth = false
 } = {}) {
 	let items = [];
 
